ecb2f75d12599d7ec984decba2c89c67112d6049
Gavin Lang <gavincollislang@gmail.com>
2024-08-30 17:36:28 -0700

    Initial Commit

---
+++ Monkey Buddy/Monkey_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Monkey_BuddyApp.swift
+//  Monkey Buddy
+//
+//  Created by Gavin Lang on 8/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Monkey_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
